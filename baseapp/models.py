30f48fcf9a3d966eb437530108e7581f069f2a45
Atmoslayer <ddsomdim@gmail.com>
2023-09-21 22:06:10 +0300

    Update models

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,32 +1,63 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.core.validators import MinValueValidator
+
+
+class MealType(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=50,
+    )
+
+    price = models.DecimalField(
+        'Цена',
+        max_digits=8,
+        decimal_places=2,
+        default=100,
+        validators=[MinValueValidator(1)],
+    )
+
+    class Meta:
+        verbose_name = 'Тип приема пищи'
+        verbose_name_plural = 'Типы приемов пищи'
+
+    def __str__(self):
+        return self.name
 
 
 class Dish(models.Model):
     name = models.CharField(
         'Название блюда',
-        max_length=50
+        max_length=50,
     )
 
     description = models.TextField(
         'Описание',
         max_length=200,
-        blank=True
+        blank=True,
     )
 
     recipe = models.TextField(
         'Рецепт',
         max_length=200,
-        blank=True
+        blank=True,
     )
 
     image = models.ImageField(
-        'Картинка'
+        'Картинка',
+        upload_to='dishes_images',
     )
 
     is_free = models.BinaryField(
         'Бесплатное',
-        default=False
+        default=False,
+    )
+
+    meal_type = models.ForeignKey(
+        MealType,
+        verbose_name='Тип приема пищи',
+        on_delete=models.CASCADE,
+        related_name='dishes',
     )
 
     class Meta:
@@ -38,20 +69,22 @@ class Dish(models.Model):
 
 
 class Ingredient(models.Model):
-
     name = models.CharField(
         'Название ингредиента',
-        max_length=50
+        max_length=50,
     )
 
     calories = models.DecimalField(
         'Количество калорий',
         decimal_places=2,
-        max_digits=7
+        max_digits=7,
     )
 
-    price = models.PositiveIntegerField(
+    price = models.DecimalField(
         'Цена ингредиента',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(1)],
     )
 
     class Meta:
@@ -67,23 +100,24 @@ class DishIngredientItem(models.Model):
         Dish,
         verbose_name='Блюдо',
         on_delete=models.CASCADE,
-        related_name='dish_items'
+        related_name='dish_items',
     )
 
     ingredient = models.ForeignKey(
         Ingredient,
         verbose_name='Ингредиент',
         on_delete=models.CASCADE,
-        related_name='ingredient_items'
+        related_name='ingredient_items',
     )
 
     quantity = models.PositiveIntegerField(
-        'Количество ингредиента'
+        'Количество ингредиента',
+        validators=[MinValueValidator(1)],
     )
 
     measurement = models.CharField(
         'Мера ингредиента',
-        max_length=50
+        max_length=50,
     )
 
     class Meta:
@@ -97,40 +131,18 @@ class DishIngredientItem(models.Model):
 class MenuCategory(models.Model):
     name = models.CharField(
         'Название',
-        max_length=50
+        max_length=50,
     )
 
     dish = models.ManyToManyField(
         Dish,
         verbose_name='Блюдо',
-        on_delete=models.CASCADE,
-        related_name='types_of_menu'
+        related_name='type_of_menu',
     )
 
     class Meta:
         verbose_name = 'Вид диеты'
-        verbose_name_plural = 'виды диеты'
-
-    def __str__(self):
-        return self.name
-
-
-class MealType(models.Model):
-    name = models.CharField(
-        'Название',
-        max_length=50
-    )
-
-    dish = models.ForeignKey(
-        Dish,
-        verbose_name='Блюдо',
-        on_delete=models.CASCADE,
-        related_name='type_of_meal'
-    )
-
-    class Meta:
-        verbose_name = 'Тип приема пищи'
-        verbose_name_plural = 'Типы приемов пищи'
+        verbose_name_plural = 'Виды диеты'
 
     def __str__(self):
         return self.name
@@ -139,13 +151,12 @@ class MealType(models.Model):
 class Allergy(models.Model):
     name = models.CharField(
         'Название',
-        max_length=50
+        max_length=50,
     )
     ingredients = models.ManyToManyField(
         Ingredient,
         verbose_name='Подходящие ингредиенты',
-        on_delete=models.CASCADE,
-        related_name='allergies'
+        related_name='allergies',
     )
 
 
@@ -154,23 +165,23 @@ class Subscription(models.Model):
         User,
         verbose_name='Пользователь',
         on_delete=models.CASCADE,
-        related_name='subscriptions'
+        related_name='subscriptions',
     )
 
     today_dishes = models.ManyToManyField(
         Dish,
         verbose_name='Сегодняшние блюда подписки',
-        related_name='subscriptions',
+        related_name='dish_subscriptions',
         null=True,
-        blank=True
+        blank=True,
     )
 
     allergies = models.ManyToManyField(
         MealType,
         verbose_name='Аллергии',
-        related_name='subscriptions',
+        related_name='allergy_subscriptions',
         null=True,
-        blank=True
+        blank=True,
     )
 
     meal_types = models.ManyToManyField(
@@ -178,19 +189,21 @@ class Subscription(models.Model):
         verbose_name='Типы приемов пищи',
         related_name='subscriptions',
         null=True,
-        blank=True
+        blank=True,
     )
 
     created_at = models.DateTimeField(
         verbose_name='Дата создания подписки',
-        auto_now_add=True
+        auto_now_add=True,
     )
     expires_at = models.DateTimeField(
         verbose_name='Дата истечения подписки',
     )
 
     portion_quantity = models.PositiveIntegerField(
-        'Количество персон'
+        'Количество персон',
+        default=1,
+        validators=[MinValueValidator(0)],
     )
 
     class Meta:
@@ -199,10 +212,4 @@ class Subscription(models.Model):
 
     def __str__(self):
         return self.user.name
-
-
-
-
-
-
 
