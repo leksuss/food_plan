bcd74bde21b6df92d6dafd410f58c835b8d53dbc
Andrey Alekseev <leksus@leksus.info>
2023-09-21 21:50:48 +0300

    create dummpy plugs for future views funcs

--- baseapp/views.py
+++ baseapp/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def order(request):
+    return render(request, 'order.html')
+
+
+def lk(request):
+    return render(request, 'lk.html')
+
+
+def dish(request, dish_id=None):
+    if dish_id is None:
+        dish = {}
+    else:
+        dish = {}
+    return render(request, 'dish.html', context=dish)
+
+
+def register(request):
+    return render(request, 'auth/register.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
