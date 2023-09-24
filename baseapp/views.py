554cf69f8beee2227f7b68a1c94cd4cdfd75c715
Andrey Alekseev <leksus@leksus.info>
2023-09-24 14:23:55 +0300

    add working order form with subscription creation

--- baseapp/views.py
+++ baseapp/views.py
@@ -1,16 +1,54 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from datetime import datetime, timedelta
+
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
+from .forms import CustomUserCreationForm, OrderForm
+from .models import Dish, Subscription, MealType
+from foodplan.settings import BASE_PRICE, BULK_DISCOUNT
 
 
 def index(request):
     return render(request, 'index.html')
 
-
+@login_required(login_url='auth')
 def order(request):
-    return render(request, 'order.html')
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            menu_category = form.cleaned_data['menu_category']
+            allergies = form.cleaned_data['allergies']
+            month_count = form.cleaned_data['month_count']
+            portion_quantity = form.cleaned_data['portion_quantity']
+            meal_type_ids = list(map(int, form.cleaned_data['meal_types']))
+
+            subscription = Subscription.objects.create(
+                user = request.user,
+                expires_at=datetime.now() + timedelta(days=month_count*30),
+                portion_quantity=portion_quantity,
+                menu_category=menu_category,
+            )
+            subscription.allergies.set(allergies)
+            subscription.meal_types.set(MealType.objects.filter(id__in=meal_type_ids))
+
+            price = BASE_PRICE * month_count * subscription.meal_types.count()
+            if month_count > 1:
+                price = int(price * (100 - BULK_DISCOUNT) / 100)
+
+            context = {
+                'subscription': subscription,
+                'price': price,
+                'month_count': month_count,
+            }
+            return render(request, 'order_confirm.html', context=context)
+    else:
+        form = OrderForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'order.html', context=context)
 
 
 @login_required(login_url='auth')
--- foodplan/settings.py
+++ foodplan/settings.py
@@ -139,3 +139,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASE_PRICE = 50  # in rubles
+BULK_DISCOUNT = 20  # in percent
